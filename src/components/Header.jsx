f9f5765b06b6dbe4149bf964f3db893e82bdda9b
saad <saad.bin@saad.com>
2024-08-13 14:51:18 +0500

    done with learning different ways of styling react componets

---
+++ src/components/Header.jsx
@@ -0,0 +1,58 @@
+import logo from '../assets/logo.png';
+import './Header.css'
+import styled from 'styled-components'
+
+const HeaderStyle = styled.header`
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+}
+
+header img {
+  object-fit: contain;
+  margin-bottom: 2rem;
+  width: 11rem;
+  height: 11rem;
+}
+
+header h1 {
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 0.4em;
+  text-align: center;
+  text-transform: uppercase;
+  color: #9a3412;
+  font-family: 'Pacifico', cursive;
+  margin: 0;
+}
+
+header p {
+  text-align: center;
+  color: #a39191;
+  margin: 0;
+}
+
+@media (min-width: 768px) {
+  header {
+    margin-bottom: 4rem;
+  }
+
+  header h1 {
+    font-size: 2.25rem;
+  }
+}
+`
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </HeaderStyle>
+  );
+}
